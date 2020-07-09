150534713ef5d54ec217cb499e5c95d1540a847c
Chris Lopez <chrislpz0822@gmail.com>
2020-07-09 16:58:09 -0600

    Proyecto de clase listo para subir a GitHub

---
+++ src/pages/components/contacto.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './generic-page.css';
+
+const Contacto = () => (
+    <div className="Page">
+        <h1>Acerca de mí: </h1>
+        <p>Desarrollador de paginas web y aplicaciones moviles. 09/08/2020</p>
+        <p>Contactame por:</p>
+        <p>Facebook: <a href="#">@Chris Lopez Hernandez</a></p>
+        <p>Twitter: <a href="#">@chrislpz09</a></p>
+        <p>Instagram: <a href="#">@cclopez0_0</a></p>
+    </div>
+)
+
+export default Contacto;
---
+++ src/pages/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './generic-page.css';
+
+const Home = () => (
+    <div className="Page">
+        <h1>Bienvenido a Lpz Video</h1>
+        <p>Lpz Video es una plataforma creada con React js</p>
+        <p>Se esta trabajando en su app movil....</p>
+    </div>
+)
+
+export default Home;
