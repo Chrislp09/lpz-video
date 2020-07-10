3f6f9510a0e6735e69676148c5d0785c3859839b
Chris Lopez <chrislpz0822@gmail.com>
2020-07-10 10:30:55 -0600

    El uso de Reditect - Switch

--- src/entries/app.js
+++ src/entries/app.js
@@ -11,7 +11,7 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { BrowserRouter, Route, Router, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import Header from '../pages/components/header';
 import NotFound from '../pages/components/not-found';
@@ -61,6 +61,7 @@ render(
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/videos" component={Videos} />
+          <Redirect from="/v" to="/videos"/>
           <Route exact path="/Contacto" component={Contacto} />
           <Route component={NotFound} />
         </Switch>
