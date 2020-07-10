4439a2c2cd690ae815db0c6d6770e98dce0282da
Chris Lopez <chrislpz0822@gmail.com>
2020-07-10 12:17:23 -0600

    Manipulando el historial utilizando Not Found

--- src/pages/components/not-found.js
+++ src/pages/components/not-found.js
@@ -2,12 +2,43 @@ import React, { PureComponent } from 'react'
 import './generic-page.css';
 
 class NotFound extends PureComponent {
+    handleRandomClick = () => {
+        const random = Math.round(Math.random() * (10 - 1) + 1);
+        this.props.history.push(`/videos?id=${random}`, {id: random}) 
+    }
+
+    handleForwardClick = () => {
+        this.props.history.goForward();
+    }
+
+    handleBackClick = () => {
+        /* this.props.history.goBack(); */
+        this.props.history.go(-1);
+    }
     render () {
         return (
             <div className="Page NotFound">
                 <h1>404</h1>
                 <h3 className="SadFace">:(</h3>
                 <h2>No hemos encontrado la pagina que buscabas...</h2>
+                <button
+                    className="Button"
+                    onClick={this.handleForwardClick}
+                >
+                    Ir a la siguiente pagina 👉
+                </button>
+                <button
+                    className="Button"
+                    onClick={this.handleBackClick}
+                >
+                    Ir a la pagina anterior 👈
+                </button>
+                <button
+                    className="Button"
+                    onClick={this.handleRandomClick}
+                >
+                    Video Ramdom 👀
+                </button>
             </div>
         )
     }
